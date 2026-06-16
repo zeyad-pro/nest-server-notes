5468c8df106a705cd247654622740a402b4686be
Zeyad <zeyadmaher177@gmail.com>
2026-06-16 11:07:40 +0300

    Enable CORS in the application

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
